92926ad2764ea14070579838056737aabf2f207d
cBombs <richie.ucsb@gmail.com>
2020-10-12 12:56:48 -0700

    combining code

--- Lab7/Lab7.c
+++ Lab7/Lab7.c
@@ -1,7 +1,7 @@
-
-/**  SPI C Seven-Segment Display Example, Written by Derek Molloy (www.derekmolloy.ie)
-*    for the book Exploring Raspberry Pi. Based on the spidev_test.c code
-*    example at www.kernel.org  */
+/**  SPI C Seven-Segment Display Example, Written by Derek Molloy
+*    COMP 462
+*    Editor: Richie Romero CastleBomber
+*/
 
 #include<stdio.h>
 #include<fcntl.h>
@@ -10,10 +10,11 @@
 #include<stdint.h>
 #include<linux/spi/spidev.h>
 #define SPI_PATH "/dev/spidev0.0"
+#include<linux/i2c.h>
+#include<linux/i2c-dev.h>
+#define BUFFER_SIZE 19
 
-// The binary data that describes the LED state for each symbol
-// A(top)         B(top right) C(bottom right)  D(bottom)
-// E(bottom left) F(top left)  G(middle)        H(dot)
+// binary data
 const unsigned char symbols[16] = {                   //(msb) HGFEDCBA (lsb)
      0b00111111, 0b00000110, 0b01011011, 0b01001111,  // 0123
      0b01100110, 0b01101101, 0b01111101, 0b00000111,  // 4567
@@ -43,10 +44,34 @@ int transfer(int fd, unsigned char send[], unsigned char rec[], int len){
    return status;
 }
 
+// the time is in the registers in decimal form
+int bcdToDec(char b) { return (b/16)*10 + (b%16); }
+
 int main(){
    int fd, i;                              // file handle and loop counter
    unsigned char null=0x00;                // sending only a single char
    uint8_t mode = 3;                       // SPI mode 3
+   int file;
+
+   printf("Starting the DS3231 test application\n");
+   if((file=open("/dev/i2c-1", O_RDWR)) < 0){
+      perror("failed to open the bus\n");
+      return 1;
+   }
+   if(ioctl(file, I2C_SLAVE, 0x68) < 0){
+      perror("Failed to connect to the sensor\n");
+      return 1;
+   }
+   char writeBuffer[1] = {0x00};
+   if(write(file, writeBuffer, 1)!=1){
+      perror("Failed to reset the read address\n");
+      return 1;
+   }
+   char buf[BUFFER_SIZE];
+   if(read(file, buf, BUFFER_SIZE)!=BUFFER_SIZE){
+      perror("Failed to read in the buffer\n");
+      return 1;
+   }
 
    // The following calls set up the SPI bus properties
    if ((fd = open(SPI_PATH, O_RDWR))<0){
@@ -63,17 +88,22 @@ int main(){
    }
    printf("SPI Mode is: %d\n", mode);
    printf("Counting in hexadecimal from 0 to F now:\n");
+
+   // Loop is here from 0x0 to 0xF (0 to 15)
    for (i=0; i<=15; i++)
    {
       // This function can send and receive data, just sending now
       if (transfer(fd, (unsigned char*) &symbols[i], &null, 1)==-1){
          perror("Failed to update the display");
          return -1;
-      }
-      printf("%4d\r", i);   // print the number in the terminal window
+
+
+      printf("%4d\r", bcdToDec(buf[2]))
+      //printf("%4d\r", i);
       fflush(stdout);       // need to flush the output, no \n
       usleep(500000);       // sleep for 500ms each loop
    }
+
    close(fd);               // close the file
    return 0;
 }
